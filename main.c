9ca6e98f7ea5c69b11a4431a9e386fbb77967c80
A. Dergachev <dergachev88@gmail.com>
2020-02-19 09:54:05 +0300

    read two ints from file

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	printf("Hello, world!\n");
+	FILE *pf = fopen("1.txt", "r");
+	if (!pf)
+	{
+		printf("Can't open file 1.txt!\n");
+		exit(1);
+	}
+	
+	int a, b;
+	fscanf(pf, "%d%d", &a, &b);
+	printf("read %d %d\n", a, b);
+	
+	fclose(pf);
 }
