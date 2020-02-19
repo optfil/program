8330ce4d3d776d9e019a12dee5f92d6d8970eb32
A. Dergachev <dergachev88@gmail.com>
2020-02-19 09:56:03 +0300

    replace reading from file with stdin

--- main.c
+++ main.c
@@ -3,16 +3,7 @@
 
 int main()
 {
-	FILE *pf = fopen("1.txt", "r");
-	if (!pf)
-	{
-		printf("Can't open file 1.txt!\n");
-		exit(1);
-	}
-	
 	int a, b;
-	fscanf(pf, "%d%d", &a, &b);
+	scanf("%d%d", &a, &b);
 	printf("read %d %d\n", a, b);
-	
-	fclose(pf);
 }
